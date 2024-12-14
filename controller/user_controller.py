5b4af99011158e6f9bfd69ba5d7a9904a3d6a60d
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-12-14 23:13:10 +0530

    inserting data in database from browser using POST method

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,13 @@
 from app import app
 from model.user_model import Usermodel
+from flask import request
 obj=Usermodel()
 @app.route("/user/getall")
 def user_getall_controller():   
     return obj.user_getall_model()
+    # return "Thiss is user page" 
+
+@app.route("/user/addOne", methods=["POST"])
+def user_addone_controller():   
+    return obj.user_addone_model(request.form)
     # return "Thiss is user page" 
--- model/user_model.py
+++ model/user_model.py
@@ -5,6 +5,7 @@ class Usermodel():
     def __init__(self):
         try:   
             self.con=mysql.connector.connect(host="localhost",user="root",password="Sandeep",database="flask_data")
+            self.con.autocommit=True
             self.cur=self.con.cursor(dictionary=True)
             print("Connection successful")
         except:
@@ -18,3 +19,7 @@ class Usermodel():
             return json.dumps(result)
         else:
             return "No data found"
+        
+    def user_addone_model(self,data):
+        self.cur.execute(f"INSERT INTO user(username,email,mobile,password) VALUES('{data['username']}','{data['email']}','{data['mobile']}','{data['password']}')")
+        return "Data added succesfully"
