a827558498e869d8b3df5551131cfd55dd5cfaa1
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-12-14 22:15:38 +0530

    database connection and returning data from database in json format

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,7 @@
 from app import app
 from model.user_model import Usermodel
 obj=Usermodel()
-@app.route("/user")
-def user():
-    return obj.userModel()
+@app.route("/user/getall")
+def user_getall_controller():   
+    return obj.user_getall_model()
     # return "Thiss is user page" 
--- model/user_model.py
+++ model/user_model.py
@@ -1,3 +1,20 @@
+import mysql.connector
+import json
+
 class Usermodel():
-    def userModel(self):
-        return "user page login"
+    def __init__(self):
+        try:   
+            self.con=mysql.connector.connect(host="localhost",user="root",password="Sandeep",database="flask_data")
+            self.cur=self.con.cursor(dictionary=True)
+            print("Connection successful")
+        except:
+            print("Error in connecting database")
+
+    def user_getall_model(self):
+        self.cur.execute("SELECT * FROM user")
+        result=self.cur.fetchall()
+        print(result)
+        if len(result)>0:
+            return json.dumps(result)
+        else:
+            return "No data found"
