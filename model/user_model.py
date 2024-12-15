a2d80c1cd4f7a427204a96d253ecf6e2348e7f27
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-12-15 10:51:11 +0530

    Generating JSON response

--- model/user_model.py
+++ model/user_model.py
@@ -18,19 +18,19 @@ class Usermodel():
         if len(result)>0:
             return json.dumps(result)
         else:
-            return "No data found"
+            return {"message":"No data found"}
         
     def user_addone_model(self,data):
         self.cur.execute(f"INSERT INTO user(username,email,mobile,password) VALUES('{data['username']}','{data['email']}','{data['mobile']}','{data['password']}')")
-        return "Data added succesfully"
+        return {"message":"Data added succesfully"}
     
     def user_updateone_model(self,data):
         self.cur.execute(f"UPDATE user SET mobile='{data['mobile']}' WHERE id='{data['id']}'")
-        return "Data updated succesfully"
+        return {"message":"Data updated succesfully"}
     
     def user_deleteone_model(self,id):
         self.cur.execute(f"DELETE FROM user  WHERE id={id}")
         if self.cur.rowcount>0:
-            return "Data deleted succesfully"
+            return {"message":"Data deleted succesfully"}
         else:
-            return "Nothing deleted"
+            return {"message":"Nothing deleted"}
